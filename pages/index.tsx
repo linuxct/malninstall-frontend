d1d4ee5d9aee1edb0011ef9a7b83478d22bcc432
linuxct <linuxct@linuxct.space>
2021-05-13 00:56:53 +0200

    2021-05-13 Test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import getConfig from "next/config";
+import { useState } from 'react';
 import Head from "next/head";
 import React from "react";
 import Layout from "../components/layout";
@@ -22,9 +23,8 @@ const Home = () => {
 };
 
 function Form() {
-  let state = {isVerified: false};
-  let captcha = React.createRef();
-  const { isVerified } = state;
+  const [disabled, setDisabled] = useState(false);
+  //setDisabled(true);
 
   const generatePackage = async event => {
     event.preventDefault()
@@ -44,24 +44,9 @@ function Form() {
     // result.user => 'Ada Lovelace'
   }
 
-  const handleChange = function(event) {
-    this.setState({isVerified: true});
-  }
-
-  const onVerifyCaptcha = function(token) {
+  function onVerifyCaptcha(token){
     console.log("Verified: " + token);
-    this.setState({isVerified: true})
-  }
-
-  const handleSubmit = function(event) {
-    event.preventDefault()
-    this.child.execute()
-  }
-
-  const handleReset = function(event) {
-    event.preventDefault()
-    this.captcha.current.resetCaptcha()
-    this.setState({isVerified: false})
+    setDisabled(false);
   }
 
   return (
@@ -76,10 +61,10 @@ function Form() {
           <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" required placeholder="com.package.name" />
         </div>
       </div>
-      <div className="md:flex md:items-center mb-6">
+      <div className="md:flex md:items-center mb-6 pl-4">
         <HCaptcha sitekey="81554bf3-7391-4691-94fe-ed8ab8a1b80e" onVerify={onVerifyCaptcha} />
       </div>
-      {isVerified && 
+      {!disabled && 
         <div className="md:flex md:items-center">
           <div className="md:w-1/3"></div>
           <div className="md:w-2/3">
