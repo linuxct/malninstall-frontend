aaee363e14eb7a3e410c105c20fbf50efaef2ebb
linuxct <linuxct@linuxct.space>
2021-05-13 23:52:26 +0200

    2021-03-15 Improve UX, add device mock

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ import axios from 'axios'
 
 const { publicRuntimeConfig } = getConfig();
 const { title } = publicRuntimeConfig.siteMetaData;
-const { apiBaseUrl, faqText } = publicRuntimeConfig;
+const { apiBaseUrl, faqText, enableMobileClientUpsell } = publicRuntimeConfig;
 
 const Home = () => {
   return (
@@ -134,7 +134,7 @@ function Form() {
           <div className="md:w-1/3">
             <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name">
               Package Name
-          </label>
+                  </label>
           </div>
           <div className="md:w-2/3">
             <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" name="packagename"
@@ -155,83 +155,88 @@ function Form() {
           </div>
         }
       </form>
-      <div id="modal">
-        <Transition.Root show={showModal} as={Fragment}>
-          <Dialog
-            as="div"
-            static
-            className="fixed z-10 inset-0 overflow-y-auto"
-            initialFocus={okButtonRef}
-            open={showModal}
-            onClose={setShowModal}
-          >
-            <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-              </Transition.Child>
+      { enableMobileClientUpsell && 
+        <section className="float-right">
+          <a href="https://play.google.com/store/apps/details?id=space.linuxct.malninstall">
+            <img className="mock-device max-w-sm" src="/images/mock.png"></img>
+          </a>
+        </section>
+      }
+      <Transition.Root show={showModal} as={Fragment}>
+        <Dialog
+          as="div"
+          static
+          className="fixed z-10 inset-0 overflow-y-auto"
+          initialFocus={okButtonRef}
+          open={showModal}
+          onClose={setShowModal}
+        >
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
 
-              {/* This element is to trick the browser into centering the modal contents. */}
-              <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
-                &#8203;
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+              &#8203;
               </span>
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                enterTo="opacity-100 translate-y-0 sm:scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              >
-                <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                  <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                    <div className="sm:flex sm:items-start">
-                      {
-                        isError ?
-                          <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                            <ExclamationIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
-                          </div>
-                          : isInfo ?
-                            <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
-                              <InformationCircleIcon className="h-6 w-6 text-blue-600" aria-hidden="true" />
-                            </div>
-                            :
-                            <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
-                              <CheckCircleIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
-                            </div>
-                      }
-                      <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                        <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900" dangerouslySetInnerHTML={{ __html: dialogTitle }}></Dialog.Title>
-                        <div className="mt-2">
-                          <p dangerouslySetInnerHTML={{ __html: dialogText }} className="text-sm text-gray-500"></p>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    {
+                      isError ?
+                        <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                          <ExclamationIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
                         </div>
+                        : isInfo ?
+                          <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
+                            <InformationCircleIcon className="h-6 w-6 text-blue-600" aria-hidden="true" />
+                          </div>
+                          :
+                          <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
+                            <CheckCircleIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
+                          </div>
+                    }
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900" dangerouslySetInnerHTML={{ __html: dialogTitle }}></Dialog.Title>
+                      <div className="mt-2">
+                        <p dangerouslySetInnerHTML={{ __html: dialogText }} className="text-sm text-gray-500"></p>
                       </div>
                     </div>
                   </div>
-                  <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                    <button
-                      type="button"
-                      className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                      onClick={() => setShowModal(false)}
-                      ref={okButtonRef}
-                    >
-                      {isInfo ? "Close" : "OK"}
-                </button>
-                  </div>
                 </div>
-              </Transition.Child>
-            </div>
-          </Dialog>
-        </Transition.Root>
-      </div>
+                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                  <button
+                    type="button"
+                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => setShowModal(false)}
+                    ref={okButtonRef}
+                  >
+                    {isInfo ? "Close" : "OK"}
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
     </div>
   )
 }
