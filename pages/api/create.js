23ea65e568495500f7d71843ec9ce99f94a9d4ca
linuxct <linuxct@linuxct.space>
2021-05-13 03:02:34 +0200

    2021-05-13 Test

--- pages/api/create.js
+++ pages/api/create.js
@@ -1,15 +1,19 @@
 import axios from 'axios'
 
 export default function handler(req, res) {
-    console.log(req.body)
     const requestData = {
         'entryChannel': 'Web',
         'safetyNetJwt': '',
         'hcaptchaClientResponse': req.body.hcaptcha,
         'packageName': req.body.packagename
     }
+
     axios.post('https://malninstall-configuration.linuxct.space/PackageCreator/GeneratePackage', requestData)
         .then(function(response) {
             res.status(200).json({ url: response.downloadUrl })
+        })
+        .catch(function(error) {
+            console.log(error);
+            res.status(403).json({ err: error })
         });
 }
