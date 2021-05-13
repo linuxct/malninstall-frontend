ca5545494bd45b05f3876433b98c5cc6f7bb3d50
linuxct <linuxct@linuxct.space>
2021-05-13 02:34:28 +0200

    2021-05-13 Test

--- pages/index.tsx
+++ pages/index.tsx
@@ -72,7 +72,7 @@ function Form() {
           </label>
         </div>
         <div className="md:w-2/3">
-          <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" name="full-package-name" 
+          <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" name="packagename" 
             value={query.packagename} onChange={handleParam()} required />
         </div>
       </div>
